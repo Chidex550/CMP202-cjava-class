426c583b0bbebcc2922b32dd511af8865ccc290f
Chidex550 <168143091+Chidex550@users.noreply.github.com>
2024-05-21 13:52:40 +0100

    Add files via upload

---
+++ main.java
@@ -0,0 +1,25 @@
+package cmp202;
+
+public class main {
+    private String name ="oha chidera";
+    private String password = "fisjxkbuie";
+
+    String getName() {
+        return name;
+    }
+
+    String getPass() {
+        return password;
+    }
+
+    void setName(String nameString) {
+        this.name = nameString;
+    }
+
+    void setPass(String passString) {
+        this.password = passString;
+    }
+
+    
+    
+}
---
+++ person.java
@@ -0,0 +1,10 @@
+package cmp202;
+public class person extends main {
+    public static void main(String[] args) {
+        person chidi = new person();
+        chidi.setName("oha chidera");
+        chidi.setPass("xyudconwo ");
+        System.out.println(chidi.getName());
+    }
+}
+     
